0a4029e327a8ad5515e7d78e93b527006dad8629
simorinka <lebrmn.37@gmail.com>
2023-05-29 12:20:40 +1000

    Change 1

--- Inf_1/Inf_1.cpp
+++ Inf_1/Inf_1.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int main() {
+	int s = 0;
 	int* arr = new int[5];
 	for (int i = 0; i < 5; i++) {
-		arr[i] = i + 1;
+		arr[i] = i;
 	}
 	cout << "Massive:\n";
 	for (int i = 0; i < 5; i++) {
 		cout << arr[i] << " ";
+		s += arr[i];
 	}
+	cout << endl;
+	cout << "sum of elements: " << s;
 	return 0;
 }
