770e137c01549d31ed9e02d7530037446fbb5435
simorinka <lebrmn.37@gmail.com>
2023-05-29 12:10:15 +1000

    exp commit

--- Inf_1/Inf_1.cpp
+++ Inf_1/Inf_1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 	int* arr = new int[5];
 	for (int i = 0; i < 5; i++) {
-		arr[i] = i;
+		arr[i] = i + 1;
 	}
 	cout << "Massive:\n";
 	for (int i = 0; i < 5; i++) {
