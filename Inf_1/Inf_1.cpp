afce530a56aa032c5900fde90a2d48ed605d2bc6
simorinka <lebrmn.37@gmail.com>
2023-05-29 11:43:00 +1000

    first commit

---
+++ Inf_1/Inf_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int* arr = new int[5];
+	for (int i = 0; i < 5; i++) {
+		arr[i] = i;
+	}
+	cout << "Massive:\n";
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
